fd22c2048586bb2cdef749f8518aa1269d63635d
PhiYerion <PhiYerion@protonmail.com>
2023-05-16 23:55:34 -0400

    Added documentation, getTotalMass, removed Tanker class

--- main.cpp
+++ main.cpp
@@ -2,17 +2,34 @@
 #include <iostream>
 #include <valarray>
 
+/**
+ * @brief Represents an engine.
+ */
 struct Engine {
-    double mass, exhaustVelocity;
-    const char* name;
-};
-struct Stage {
-    double dryMass,                                 // does not include engine mass
-    fuelMass;
-    Engine engine;
-    double deltaV;
+    double mass;               /**< Mass of the engine. */
+    double exhaustVelocity;    /**< Exhaust velocity of the engine. */
+    const char* name;          /**< Name of the engine. */
 };
 
+/**
+ * @brief Represents a stage of a spaceship.
+ *
+ * Any loss in non-fuel mass mid-stage will not be accounted for.
+ */
+struct Stage {
+    Engine engine;             /**< Engine used in the stage. */
+    double deltaV,             /**< Delta-V of the stage. */
+           dryMass,            /**< Dry mass of the stage (excluding engine mass). */
+           fuelMass,           /**< Fuel mass of the stage. */
+           totalMass = 0;      /**< Total mass of the stage (including engine mass). */
+};
+
+/**
+ * @brief Spaceship class with full functionality.
+ *
+ * This class provides getters and setters for accessing and modifying the spaceship's attributes.
+ * Directly setting variables will produce inaccurate measurements, so they are protected.
+ */
 class SpaceShip {
 public:
     SpaceShip() {
@@ -21,58 +38,19 @@ public:
         stages = {};
     }
 
+    /**
+      * @brief Returns the vector of stages.
+      * @return Vector of stages.
+      */
     std::vector<Stage> getStages () {
         return stages;
     }
-    Engine getEngine (Stage* stage) {
-        stage -> engine;
-    }
-    double getStageDryMass (Stage* stage) {
-        return stage -> dryMass;
-    }
-    double getStageFuelMass (Stage* stage) {
-        return stage -> fuelMass;
-    }
-    double getStageDeltaV (Stage* stage) {
-        return stage -> deltaV;
-    }
-    double getMass () {
-        return mass;
-    }
-    double getDeltaV () {
-        return deltaV;
-    }
-    void setStageDryMass (Stage* stage, const double newMass) {
-        mass += newMass - stage -> dryMass;
-        stage -> dryMass = newMass;
-        genDeltaV(stage);
-    }
-    void setStageFuelMass (Stage* stage, const double newMass) {
-        mass += newMass - stage -> fuelMass;
-        stage -> fuelMass = newMass;
-        genDeltaV(stage);
-    }
-    void setStageEngine (Stage* stage, const Engine newEngine) {
-        mass += newEngine.mass - stage -> engine.mass;
-        stage -> engine = newEngine;
-        genDeltaV(stage);
-    }
 
-    void addStage (const double dryMass, const double fuelMass, const Engine engine, const int index = -1) {
-        if (index != -1) {
-            stages.insert(stages.begin() + index, {dryMass, fuelMass, engine, 0});
-        } else {
-            stages.push_back({dryMass, fuelMass, engine, 0});
-        }
-        mass += dryMass + fuelMass + engine.mass;
-        genDeltaV();
-    }
-
-protected:
-    std::vector<Stage> stages;
-    double mass;
-    double deltaV;
-
+    /**
+     * @brief Returns the mass of all stages above the specified stage + the specified stage's mass.
+     * @param inputStage The stage.
+     * @return Mass of rocket during this stage prior to any burn or mass loss.
+     */
     double getRemainingMass (const Stage& inputStage) {
         double remainingMass = mass;
         for (const auto& stage : stages) {
@@ -84,6 +62,130 @@ protected:
         return remainingMass;
     }
 
+    /**
+    * @brief Returns the engine of a stage.
+    * @param stage Pointer to the stage.
+    * @return Engine of the stage.
+    */
+    Engine getEngine (Stage* stage) {
+        stage -> engine;
+    }
+
+    /**
+    * @brief Returns the dry mass of a stage.
+    * @param stage Pointer to the stage.
+    * @return Dry mass of the stage.
+    */
+    double getStageDryMass(Stage* stage) {
+        return stage->dryMass;
+    }
+
+    /**
+     * @brief Returns the fuel mass of a stage.
+     * @param stage Pointer to the stage.
+     * @return Fuel mass of the stage.
+     */
+    double getStageFuelMass(Stage* stage) {
+        return stage->fuelMass;
+    }
+
+    /**
+     * @brief Returns the delta-V of a stage.
+     * @param stage Pointer to the stage.
+     * @return Delta-V of the stage.
+     */
+    double getStageDeltaV(Stage* stage) {
+        return stage->deltaV;
+    }
+
+    /**
+     * @brief Returns the total mass of a stage.
+     * @param stage Pointer to the stage.
+     * @return Total mass of the stage.
+     */
+    double getStrageTotalMass(Stage* stage) {
+        return stage->totalMass;
+    }
+
+    /**
+     * @brief Returns the total mass of the spaceship.
+     * @return Total mass of the spaceship.
+     */
+    double getMass() {
+        return mass;
+    }
+
+    /**
+     * @brief Returns the total delta-V of the spaceship.
+     * @return Total delta-V of the spaceship.
+     */
+    double getDeltaV() {
+        return deltaV;
+    }
+
+    /**
+     * @brief Sets the dry mass of a stage.
+     * @param stage Pointer to the stage.
+     * @param newMass The new dry mass.
+     */
+    void setStageDryMass(Stage* stage, const double newMass) {
+        mass += newMass - stage->dryMass;
+        stage->dryMass = newMass;
+        genDeltaV(stage);
+    }
+
+    /**
+     * @brief Sets the fuel mass of a stage.
+     * @param stage Pointer to the stage.
+     * @param newMass The new fuel mass.
+     */
+    void setStageFuelMass(Stage* stage, const double newMass) {
+        mass += newMass - stage->fuelMass;
+        stage->fuelMass = newMass;
+        genDeltaV(stage);
+    }
+
+    /**
+     * @brief Sets the engine of a stage.
+     * @param stage Pointer to the stage.
+     * @param newEngine The new engine.
+     */
+    void setStageEngine(Stage* stage, const Engine newEngine) {
+        mass += newEngine.mass - stage->engine.mass;
+        stage->engine = newEngine;
+        genDeltaV(stage);
+    }
+
+    /**
+     * @brief Adds a stage to the spaceship.
+     * @param dryMass The dry mass of the stage.
+     * @param fuelMass The fuel mass of the stage.
+     * @param engine The engine used in the stage.
+     * @param index The index at which to insert the stage (optional).
+     */
+    void addStage(const double dryMass, const double fuelMass, const Engine engine, const int index = -1) {
+        if (index != -1) {
+            stages.insert(stages.begin() + index, {engine, 0, dryMass, fuelMass, dryMass + fuelMass + engine.mass});
+        } else {
+            stages.push_back({engine, 0, dryMass, fuelMass, dryMass + fuelMass + engine.mass});
+            std::cerr << "Warning: index not specified for addStage, appending to end of stages\n";
+        }
+        mass += dryMass + fuelMass + engine.mass;
+        genDeltaV();
+    }
+
+protected:
+    std::vector<Stage> stages; /**< Vector of stages. */
+    double mass; /**< Total mass of the spaceship. */
+    double deltaV; /**< Total delta-V of the spaceship. */
+
+    /**
+     * @brief Generates the delta-V for the spaceship or a specific stage.
+     * @param inpStage Pointer to the input stage (optional).
+     *
+     * If inpStage is specified, it calculates the delta-V only for stages before the new stage.
+     * Otherwise, it calculates the delta-V for all stages.
+     */
     void genDeltaV (Stage* inpStage = nullptr) {       // for addStage, this should implement only calcs on stages before new
         if (inpStage) {
             double remainingMass = getRemainingMass(*inpStage);
@@ -99,33 +201,25 @@ protected:
         }
     }
 
-
 };
-
-class Tanker : SpaceShip {
-public:
-    Tanker () {
-        mass = 0;
-        deltaV = 0;
-        stages = {};
-    }
-};
-
 
 int main () {
     auto* ship = new SpaceShip();
-    ship->addStage(100, 1, {100, 100, "test"});
+    ship->addStage(100, 1, {100, 100, "S1"});
+    ship->addStage(50, 20, {5, 1000, "S0"});
     printf("DeltaV: %f\n", ship->getDeltaV());
     printf("Mass: %f\n", ship->getMass());
     uint i = 0;
     for (const auto &stage : ship->getStages()) {
         printf("Stage %u:\n", i);
+        printf("\tTotal Stage Mass: %fkg\n", stage.totalMass);
+        printf("\tTotal Mass for remaining stages: %fkg\n", ship->getRemainingMass(stage));
+        printf("\tDeltaV: %fm/s\n", stage.deltaV);
         printf("\tDryMass: %fkg\n", stage.dryMass);
         printf("\tFuelMass: %fkg\n", stage.fuelMass);
         printf("\tEngine %s:\n", stage.engine.name);
         printf("\t\tMass: %fkg\n", stage.engine.mass);
         printf("\t\tExhaustVelocity: %fm/s\n", stage.engine.exhaustVelocity);
-        printf("\tDeltaV: %fm/s\n", stage.deltaV);
     }
     free(ship);
     return 1;
