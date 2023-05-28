c19f025d84f53dec36a721e9c45045c433ca631c
PhiYerion <PhiYerion@protonmail.com>
2023-05-27 21:08:06 -0400

    Minor additions

--- main.cpp
+++ main.cpp
@@ -6,7 +6,10 @@
 int main () {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_real_distribution<long double> valRange(1, 1'000'000'000.0);
+    std::uniform_int_distribution<uint> magnitude(2, 10);
+    std::uniform_real_distribution<long double> valRange(0.001, 10);
+
+    auto rnd = [&valRange, &gen, &magnitude]() {return valRange(gen) * pow(10, magnitude(gen));};
 
     SpaceShipHandler handler(1024);
     std::vector<SpaceShipWrapper*> ships;
@@ -15,8 +18,8 @@ int main () {
         auto ship = handler.addShip();
         for (int j = 0; j < 5; j++) {
             auto engineName = "S" + std::to_string(i) + "." + std::to_string(j);
-            handler.createEngine(engineName, valRange(gen), valRange(gen));
-            ship->addStage(valRange(gen), valRange(gen), handler.getEngine(engineName));
+            handler.createEngine(engineName, rnd(), rnd());
+            ship->addStage(rnd(), rnd(), handler.getEngine(engineName));
             ship->printStats();
         }
     }
