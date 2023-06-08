4350f4968ce958b3789bd53c2a7a9398327f7328
PhiYerion <PhiYerion@protonmail.com>
2023-06-07 20:32:51 -0400

    template for gui

--- main.cpp
+++ main.cpp
@@ -3,7 +3,20 @@
 #include <random>
 #include <cstdlib>
 #include "SpaceShipHandler.h"
+#include <QApplication>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QtWidgets>
+#include <QtCore>
+#include <QtGui>
+#include "Window.cpp"
 
-int main () {
-    
+int main(int argc, char *argv[]) {
+    SpaceShipHandler handler(1024);
+    QApplication a(argc, argv);
+
+    MyWindow w(&handler);
+    w.show();
+
+    return a.exec();
 }
